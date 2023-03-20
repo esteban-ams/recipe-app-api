7b075e2c3735284cd7e3b54e533e1abacdfda089
esteban-ams <esteban.ams95@gmail.com>
2023-03-20 15:01:20 -0300

    add database and config pgsql

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator funtions
+"""
+
+def add(x ,y):
+    """Add x and y and returns result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
+    # return 0
