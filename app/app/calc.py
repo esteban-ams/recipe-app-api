5c2102b2207ad12bff1c322f7b58024a2bf5a3a3
esteban-ams <esteban.ams95@gmail.com>
2023-03-21 02:13:28 -0300

    configure compose and check to use "wait_for_db"

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator funtions
 """
 
-def add(x ,y):
+
+def add(x, y):
     """Add x and y and returns result."""
     return x + y
+
 
 def subtract(x, y):
     """Subtract x from y and return result."""
