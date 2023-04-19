27c86c5c518f30198071f5c9550d5a3b32518f95
esteban-ams <esteban.ams95@gmail.com>
2023-04-19 11:11:12 -0400

    user serializers and creation API

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
 
     #Project apps
     'core',
+    'user',
+
+    # Third party apps
+    'rest_framework',
+    'drf_spectacular',
+
 ]
 
 MIDDLEWARE = [
@@ -131,3 +137,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
